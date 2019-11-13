5a32c3c4547cd50a66a70d2f794c120ef487a0cb
Capitan Pinto <santiagopfisica@gmail.com>
2019-11-13 00:36:30 +0000

    asd

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'basicpages/home'
+  get 'basicpages/help'
+  root 'services#home'
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users
+  resource :services
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_12_232938) do
+ActiveRecord::Schema.define(version: 2019_11_12_233925) do
+
+  create_table "services", force: :cascade do |t|
+    t.float "price"
+    t.integer "user_id", null: false
+    t.boolean "paid"
+    t.date "meeting_time"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index "\"user_id\", \"maid_id\"", name: "index_services_on_user_id_and_maid_id"
+    t.index ["user_id"], name: "index_services_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -20,8 +31,11 @@ ActiveRecord::Schema.define(version: 2019_11_12_232938) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
+    t.boolean "maid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "services", "users"
 end
